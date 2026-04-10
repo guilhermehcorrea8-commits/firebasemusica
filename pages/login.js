83b070ad6b1a49bf000ee90ca9e03c6ce04b0dfc
guilhermehcorrea8 <guilherme.h.correa8@aluno.senai.br>
2026-04-10 09:37:35 -0300

    twoP-V1.1

--- pages/login.js
+++ pages/login.js
@@ -5,25 +5,33 @@ import {
   onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-auth.js";
 
+/* ELEMENTOS */
 const email = document.getElementById("email");
 const senha = document.getElementById("senha");
+const btnLogin = document.getElementById("btnLogin");
+const btnRegister = document.getElementById("btnRegister");
 
+/* LOGIN */
 btnLogin.onclick = async () => {
   try {
     await signInWithEmailAndPassword(auth, email.value, senha.value);
   } catch (e) {
-    alert(e.message);
+    alert("Erro: " + e.message);
   }
 };
 
+/* CADASTRO */
 btnRegister.onclick = async () => {
   try {
     await createUserWithEmailAndPassword(auth, email.value, senha.value);
   } catch (e) {
-    alert(e.message);
+    alert("Erro: " + e.message);
   }
 };
 
+/* REDIRECIONAMENTO */
 onAuthStateChanged(auth, user => {
-  if (user) window.location.href = "index.html";
+  if (user) {
+    window.location.href = "index.html";
+  }
 });
